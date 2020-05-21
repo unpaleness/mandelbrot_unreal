33a073b2e107ab44c99b8e9efeb15326c383ef64
unpaleness <generalissimusunpaleness@gmail.com>
2020-05-22 02:10:36 +0300

    Init commit

---
+++ Source/Mandelbrot/MandelbrotGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MandelbrotGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MANDELBROT_API AMandelbrotGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
